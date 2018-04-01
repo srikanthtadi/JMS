af6c6d4f7f719d2f41ef5f78562c6d0439281aa5
srika <srika@DESKTOP-TFTAOD8>
2018-04-01 14:45:28 +0530

    jms

---
+++ jms_hello_artifact/src/main/java/com/Consumer.java
@@ -0,0 +1,94 @@
+package com;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Consumer {
+
+
+	private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+
+
+	public static void main(String[] args) {
+
+		ConnectionFactory cf = null;
+		Connection c = null;
+
+
+		Session s = null;
+
+		try {
+			cf = new ActiveMQConnectionFactory(url);
+			c = cf.createConnection();
+			
+			
+			s = c.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+			Destination d = s.createQueue("jms/fristQueue");
+
+			MessageConsumer mcon = s.createConsumer(d);
+			
+			mcon.setMessageListener(new Listener());
+			c.start();
+			
+			while(true)
+				Thread.sleep(10000);
+			 
+			
+		
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		finally {
+
+			if(c != null)
+				try {
+					c.close();
+				} catch (JMSException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+		}
+
+	}
+
+}
+
+
+class Listener implements MessageListener{
+
+	public void onMessage(Message m) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("inside on message");
+		if(m instanceof TextMessage )
+		{
+			TextMessage  t = (TextMessage) m;
+			
+			try {
+				System.out.println(t.getText());
+			} catch (JMSException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+}
---
+++ jms_hello_artifact/src/main/java/com/Produer.java
@@ -0,0 +1,62 @@
+package com;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Produer {
+	
+private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+	
+	
+ public static void main(String[] args)  {
+	 
+	 ConnectionFactory cf = null;
+	 Connection c = null;
+	 
+	 
+	 Session s = null;
+	 
+	 try {
+		  cf = new ActiveMQConnectionFactory(url);
+		  c = cf.createConnection();
+		 c.start();
+		 
+		  s = c.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		 
+	 Destination d = s.createQueue("jms/fristQueue");
+	 
+	MessageProducer p =  s.createProducer(d);
+	
+	TextMessage  t  = s.createTextMessage("Hi.. This is fifth  message");
+	
+	p.send(t);
+	System.out.println("message sent");
+	 }
+	 catch(Exception e)
+	 {
+		 e.printStackTrace();
+	 }
+	 
+	 finally {
+		 
+		 	if(c != null)
+				try {
+					c.close();
+				} catch (JMSException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		  
+	 }
+	 
+}
+
+}
